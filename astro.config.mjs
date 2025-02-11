b29116efe9557f52b882b0fc7240e4f62c4f0996
Jurajj8 <kristak.juraj@gmail.com>
2025-02-11 18:23:54 +0100

    GeneralUpgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,22 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/',
   output : "static",
 
-  vite:{
-    plugins: [
-      tailwindcss()
-    ]
+  vite: {
+    plugins: [tailwindcss()],
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: (assetInfo) => {
+            if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+              return 'style.css'; // Vynúti názov style.css pre CSS
+            }
+            return '_astro/[name]-[hash][extname]';
+          },
+        },
+      },
+    },
   }
 });
